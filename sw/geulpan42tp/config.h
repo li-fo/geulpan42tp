2aaf9b7bd9dc2f17b2fad9711a77d069023c7571
ethan park <pgh@cofl.co.kr>
2024-09-16 04:39:13 +0900

    mod led config and rebuild firmware for PCB Version

--- sw/geulpan42tp/config.h
+++ sw/geulpan42tp/config.h
@@ -23,6 +23,7 @@
 #define WS2812_PIO_USE_PIO2 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
 #define WS2812_DI_PIN GP23  // The pin connected to the data pin of the LEDs
 #define RGBLIGHT_LED_COUNT 1 // The number of LEDs connected  
+#define RGBLED_NUM 1 
 
 // RGBLIGHT
 #define RGBLIGHT_SPLIT 
